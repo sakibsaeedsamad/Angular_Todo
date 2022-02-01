ed5aefc0d8220ce11f286de1db45e65d35d84412
sakibssamad <sssakib.info@gmail.com>
2022-02-01 17:30:01 +0600

    add todo class, use pipe

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {
+
+  }
+}
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,9 +18,13 @@ import { Component, OnInit } from '@angular/core';
 export class ListTodosComponent implements OnInit {
 
   todos = [
-    {id:1,description:"Coding"},
-    {id:2,description:"Dancing"},
-    {id:3,description:"singing"},
+    new Todo(1,"Coding",false,new Date()),
+    new Todo(2,"Dancing",false,new Date()),
+    new Todo(3,"Singing",false,new Date())
+
+    // { id: 1, description: "" },
+    // { id: 2, description: "" },
+    // { id: 3, description: "" },
   ]
   // todo ={
   //   id:1,
